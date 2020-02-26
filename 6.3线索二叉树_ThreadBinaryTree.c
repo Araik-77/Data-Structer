c1619cf2326157af5073366e544038f755f3d8bf
Araik-77 <1091977443@qq.com>
2020-02-26 21:48:05 +0800

    “.增加了链树和线索二叉树“

--- 6.3线索二叉树_ThreadBinaryTree.c
+++ 6.3线索二叉树_ThreadBinaryTree.c
@@ -10,7 +10,7 @@
 #define TRUE 1
 #define FALSE 0
 
-#define MAXSIZE 100 /* 存储空间初始分配量 */
+#define MAXSIZE 100 /*  存储空间初始分配量 */
 
 typedef int Status;	/* Status是函数的类型,其值是函数结果状态代码,如OK等 */
 typedef char TElemType;
